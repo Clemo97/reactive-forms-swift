47eb392e9834779b4e50b7e4884093c9b07fb9fc
clemo97 <lumumbaclement@gmail.com>
2025-09-03 13:49:06 +0300

    Initial Commit

---
+++ reactive-forms/reactive_formsApp.swift
@@ -0,0 +1,17 @@
+//
+//  reactive_formsApp.swift
+//  reactive-forms
+//
+//  Created by Clement Lumumba on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct reactive_formsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reactive-formsTests/reactive_formsTests.swift
@@ -0,0 +1,16 @@
+//
+//  reactive_formsTests.swift
+//  reactive-formsTests
+//
+//  Created by Clement Lumumba on 03/09/2025.
+//
+
+import Testing
+
+struct reactive_formsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
